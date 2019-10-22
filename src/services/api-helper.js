3c79eb6936e613be4e9b258d3dc063c29c1f8f5c
Ben Grzybowski <bengrzybowski@Bens-MacBook-Pro.local>
2019-10-22 13:38:42 -0400

    added local storage

--- src/services/api-helper.js
+++ src/services/api-helper.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
-// const baseUrl = `https://cloud.iexapis.com/stable/`;
-//https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=sony&apikey=55X1URWO4KSROPDF
 
 export const getStockQuote = async (ticker) => {
-  // const url = `https://cloud.iexapis.com/stable/tops?token=pk_210068cb77984edcac835b9f829e946d&symbols=PRU`;
   const url = `https://cloud.iexapis.com/stable/stock/${ticker}/quote?token=pk_210068cb77984edcac835b9f829e946d`;
   const resp = await axios.get(url);
   return resp.data;
-  //  {"symbol":"PRU","companyName":"Prudential Financial, Inc.","primaryExchange":"New York Stock Exchange","calculationPrice":"close","open":89.39,"openTime":1571146200540,"close":89.95,"closeTime":1571169669507,"high":90.93,"low":88.91,"latestPrice":89.95,"latestSource":"Close","latestTime":"October 15, 2019","latestUpdate":1571169669507,"latestVolume":1466435,"iexRealtimePrice":89.95,"iexRealtimeSize":200,"iexLastUpdated":1571169598866,"delayedPrice":89.95,"delayedPriceTime":1571170200011,"extendedPrice":90.1,"extendedChange":0.15,"extendedChangePercent":0.00167,"extendedPriceTime":1571178600002,"previousClose":89.34,"previousVolume":1149727,"change":0.61,"changePercent":0.00683,"volume":1466435,"iexMarketPercent":0.021228353114867007,"iexVolume":31130,"avgTotalVolume":2309436,"iexBidPrice":0,"iexBidSize":0,"iexAskPrice":0,"iexAskSize":0,"marketCap":36159900000,"peRatio":9.03,"week52High":106.39,"week52Low":75.6,"ytdChange":0.087512,"lastTradeTime":1571169599577,"isUSMarketOpen":false}
 }
 export const getLogo = async (ticker) => {
   const url = `https://cloud.iexapis.com/stable/stock/${ticker}/logo?token=pk_210068cb77984edcac835b9f829e946d`;
@@ -30,6 +26,21 @@ export const getNews = async (ticker) => {
   return resp.data;
 }
 export const getSymbols = async () => {
+  debugger;
+  if (typeof (Storage) !== "undefined") {
+    if (localStorage.symbols !== "undefined") {
+      return JSON.parse(localStorage.symbols);
+    } else {
+      const resp = await getSymbolsInternal();
+      localStorage.symbols = JSON.stringify(resp);
+      return resp;
+    }
+  } else {
+    return await getSymbolsInternal();
+  }
+
+}
+const getSymbolsInternal = async () => {
   const url = `https://cloud.iexapis.com/stable/ref-data/symbols?token=pk_210068cb77984edcac835b9f829e946d`;
   const resp = await axios.get(url);
   return resp.data;
