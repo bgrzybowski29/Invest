0d605cfebf8e18069d9451a5e593d54b2d0c7580
Ben Grzybowski <bengrzybowski@Bens-MacBook-Pro.local>
2019-10-24 11:34:39 -0400

    test automate

--- automate.sh
+++ automate.sh
@@ -1,9 +1,6 @@
 git add .
-echo "ran add"
 git commit -m "$1"
-echo "ran commit"
 git push origin master
-echo "ran push"
 read -p "Press [Enter] key to start build and deploy..."
 npm run build
 cd build
